13348e635aaf035202dfdc51531a288f06591d38
Shaina G <shainagtherapy@gmail.com>
2025-07-08 14:22:06 -0700

    more revisions & progress  7/8

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
-const colors = ['blue', 'yellow', 'green', 'red'];
+const colorsArray = ['blue', 'yellow', 'green', 'red'];
 // push random to add to array each turn?
 
 /*-------------------------------- Variables --------------------------------*/
@@ -12,19 +12,17 @@ const colors = ['blue', 'yellow', 'green', 'red'];
 // let player;
 // let winner;
 // message;
-let sequence = colors[''];
-let playerSequence;
+// random color?
+let sequence = [''];
+let playerSequence = [];
 let isPlayerTurn = false;
-console.log(sequence)
-console.log(isPlayerTurn)
 
 /*------------------------ Cached Element References ------------------------*/
 
 const colorButtons = document.querySelectorAll('.color');
-const messageStatus = document.querySelector('#message');
+// const messageStatus = document.querySelector('#message');
 const startButton = document.querySelector('#start')
-console.log(messageStatus)
-console.log(startButton)
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 // buttonColors.addEventListener('click', (event)) => {
@@ -32,9 +30,9 @@ console.log(startButton)
 // }
 
 // click event for button colors:
-// buttonColors.addEventListener('click', colorClick);
+// colorButtons.addEventListener('click', colorClick);
 
-// function colorClick(event) {
+// function flashing(color)) {
 //     const colorIndex = event.target.id;
 //     currentInput += buttonColors;
 // }
@@ -45,18 +43,63 @@ console.log(startButton)
 
 /*-------------------------------- Functions --------------------------------*/
 
-// main sequence loop
-// function getRandom(randomColor) {
-//     let randomColor = Math.floor(Math.random() * buttonColors.length);
-//     getRandom.push(buttonColors)
-    // const randomIndex = // equation for random color?
-    // renturn buttonColors[randomIndex];
+// flash function:
+// function flash(color) {
+//     const button = document.getElementById(color);
+//     button.classList.add('active');
+
 // }
-// console.log(randomColor)
+
+// function playSequence() {
+//     let delay = 0;
+
+//     sequence.forEach((color, index)) => {
+//         setTimeout(() => {
+//             flash(color);
+
+//         })
+//     })
+// }
+
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+    sequence = [];
+    playerSequence = [""];
+    isPlayerTurn = false;
+   // messageStatus= 'Watch the pattern...'; *********** something isn't working with message
+    nextRound(); //defined below
+}
+
+function nextRound() {
+    const randomColor = colorsArray[Math.floor(Math.random() * colorsArray.length)];
+    sequence.push(randomColor);
+    playerSequence = [];
+    isPlayerTurn = false;
+    messageStatus = "Repeat the sequence...";
+   // playSequence(); still needs defined;
+}
+
+// main sequence loop
+// function randomColor() {
+//     let randomColor = Math.floor(Math.random() * colorsArray.length);
+//     randomColor.push(randomColor)
+//     // const randomIndex = // equation for random color?
+//     // return colorsArray[randomIndex];
+// }
+// console.log(colorsArray) //<----not defined?
+
+// function sequence() {
+//     const randomColor = colorsArray[Math.floor(Math.random() * colorsArray.length)];
+//     sequence.push(randomColor);
+//     playerSequence = [""];
+//     isPlayerTurn = false;
+// }
+// console.log(sequence)
 // flash function:
 // function buttonFlash() {
 // }
 
 // sound function:
 // function playSound () { ***REFER TO DOM LEVEL UP
-// }
+// } 
